8e8d1cc184460d97f31ddb0360e389d6e3f3368e
tmulle <tmulle@gmail.com>
2021-08-06 12:00:57 -0400

    Added readme and more ways to configure mode between ipv4 and ipv6

--- src/main/java/com/mycompany/vertxnettyip6udptester/Tester.java
+++ src/main/java/com/mycompany/vertxnettyip6udptester/Tester.java
@@ -1,6 +1,8 @@
 package com.mycompany.vertxnettyip6udptester;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
@@ -15,72 +17,112 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Tester class to show a possible issue with Netty/Vertx and sending IP6
- * UDP traffic on MacOS.
- * 
- * It appears that the scope-id is being stripped off during the send 
- * at least that's what the Netty debug output looks like
- * 
- * This code was thrown together and is not optimal but it works on
- * Linux and Windows
- * 
- * I have code that works using standard Java NIO classes which makes me
- * think it might be a Netty issue
- * 
- * 
+ * Tester class to show a possible issue with Netty/Vertx and sending IP6 UDP
+ * traffic on MacOS.
+ *
+ * It appears that the scope-id is being stripped off during the send at least
+ * that's what the Netty debug output looks like
+ *
+ * This code was thrown together and is not optimal but it works on Linux and
+ * Windows
+ *
+ * I have code that works using standard Java NIO classes which makes me think
+ * it might be a Netty issue
+ *
+ *
  * This code simulates a device discovery mechanism. The Device class will
- * listen to the MULTICAST_GROUP for a discovery message and then will
- * read the port it needs to send a UDP response back to.
- * 
+ * listen to the MULTICAST_GROUP for a discovery message and then will read the
+ * port it needs to send a UDP response back to.
+ *
  * @author tmulle
  */
 public class Tester {
-    
-    private static final String INTERFACE;
-    private static final String MULTICAST_GROUP = "FF02::1";
-    private static final int LISTEN_PORT;
-    private static final String LISTEN_ALL_INTERFACES = "::";
-    
-    // Specify these to suit your environment
-    static {
-        INTERFACE = System.getProperty("net.interface", "en0");
-        LISTEN_PORT = Integer.parseInt(System.getProperty("net.listen_port", "35056"));
-    }
-   
+
+    private static final Logger LOG = LoggerFactory.getLogger(Tester.class);
+
+    private static String INTERFACE;
+    private static String MULTICAST_GROUP;
+    private static String LISTEN_ALL_INTERFACE;
+    private static int LISTEN_PORT;
+    private static IP_MODE MODE;
+
+    // Which mode
+    private static enum IP_MODE {
+        IPv4, IPv6
+    };
+
     /**
      * Main
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
+
+        // Setup the variables
+        MODE = IP_MODE.valueOf(System.getProperty("net.ip_mode", "IPv6"));
+        INTERFACE = System.getProperty("net.interface", "en0");
+        LISTEN_PORT = Integer.parseInt(System.getProperty("net.listen_port", "35056"));
+
+        // Which mode?
+        switch (MODE) {
+            case IPv4: {
+                MULTICAST_GROUP = "224.0.0.224";
+                LISTEN_ALL_INTERFACE = "0.0.0.0";
+                break;
+            }
+            case IPv6: {
+                MULTICAST_GROUP = "FF02::1";
+                LISTEN_ALL_INTERFACE = "::";
+                break;
+            }
+        }
+
+        LOG.info("*** Starting Variables ***");
+        LOG.info("Network Mode = {}", MODE);
+        LOG.info("Network Interface = {}", INTERFACE);
+        LOG.info("Netork Listen All Interface = {}", LISTEN_ALL_INTERFACE);
+        LOG.info("Network Multicast Group = {}", MULTICAST_GROUP);
+        LOG.info("Network Port = {}", LISTEN_PORT);
+        LOG.info("**************************");
+
         Vertx vertx = Vertx.vertx();
 
         // Deploy the receive then the sender
-        vertx.deployVerticle(new Device());
-        vertx.deployVerticle(new Sender());
-        
+        Future<String> deviceVerticle = vertx.deployVerticle(new Device());
+        Future<String> senderVerticle = vertx.deployVerticle(new Sender());
+
+        CompositeFuture.all(deviceVerticle, senderVerticle)
+                .onFailure(error -> {
+                    LOG.error("Failed to startup", error);
+                    System.exit(-1);
+                });
+
     }
-    
+
     /**
-     * This class will send repeatedly a simulated discovery request
-     * and the Device class will respond back on the port this class
-     * put in the request message.
+     * This class will send repeatedly a simulated discovery request and the
+     * Device class will respond back on the port this class put in the request
+     * message.
      */
     static class Sender extends AbstractVerticle {
+
         private final static Logger LOG = LoggerFactory.getLogger(Sender.class);
-        
+
         @Override
         public void start(Promise<Void> startPromise) throws Exception {
 
             // Create the options 
             DatagramSocketOptions options = new DatagramSocketOptions()
                     .setReuseAddress(true)
-                    .setIpV6(true)
-                    .setReusePort(true)
-                    .setMulticastNetworkInterface(INTERFACE);
+                    .setReusePort(true);
+
+            // Enable IPv6?
+            if (MODE == IP_MODE.IPv6) {
+                options.setIpV6(true);
+            }
 
             // Create the socket to which we will listen on Multicast messages
-            DatagramSocket socket6 = vertx.createDatagramSocket(options);
+            DatagramSocket socket = vertx.createDatagramSocket(options);
 
             // Create the socket to which we will listen for Discovery replies
             DatagramSocket uniSocket = vertx.createDatagramSocket(options);
@@ -93,34 +135,34 @@ public class Tester {
             JsonObject payload = new JsonObject();
             payload.put("action", "REQUEST");
             payload.put("responsePort", uniSocket.localAddress().port());
-            
+
             // Add the data
             Buffer buffer = Buffer.buffer(payload.encode());
 
             // Send every 5 seconds - Just for fun
             LOG.info("Starting to send Discovery Requests every 5 seconds");
+
             vertx.setPeriodic(5000, id -> {
-                int count = 0;
-                LOG.info("Sending request #{}", ++count);
-                socket6.send(buffer, LISTEN_PORT, MULTICAST_GROUP)
+                LOG.info("Sending request...");
+                socket.send(buffer, LISTEN_PORT, MULTICAST_GROUP)
                         .onSuccess(v -> LOG.info("Request sent successfully"))
                         .onFailure(Throwable::printStackTrace);
             });
-            
+
         }
-        
+
         /**
          * Listens on a ephemeral port which will be used to receive the
          * discovery responses
-         * 
+         *
          * @param startPromise
-         * @param socket 
+         * @param socket
          */
         private void setupUnicastListener(Promise<Void> startPromise, DatagramSocket socket) {
-            socket.listen(0, LISTEN_ALL_INTERFACES)
-                    .onSuccess(result -> LOG.info("Listening for Unicast message on IPV6 @ {}", result.localAddress()))
+            socket.listen(0, LISTEN_ALL_INTERFACE)
+                    .onSuccess(result -> LOG.info("Listening for Unicast message on {} @ {}", MODE, result.localAddress()))
                     .onFailure(startPromise::fail);
-            
+
             // Handle the discovery reponses
             socket.handler(this::processDiscoveryResponse);
         }
@@ -138,47 +180,51 @@ public class Tester {
      * discovery request message using the port contained in the request message
      */
     static class Device extends AbstractVerticle {
-        
+
         private final static Logger LOG = LoggerFactory.getLogger(Device.class);
-        
-        private DatagramSocket socket6;
+
+        private DatagramSocket socket;
         private String id;
-        
+
         public Device() {
             id = UUID.randomUUID().toString();
         }
-        
+
         @Override
         public void start(Promise<Void> startPromise) throws Exception {
 
             // Create the options 
             DatagramSocketOptions options = new DatagramSocketOptions()
                     .setReuseAddress(true)
-                    .setIpV6(true)
                     .setLogActivity(true)
                     .setReusePort(true);
 
+            // Enable IPv6?
+            if (MODE == IP_MODE.IPv6) {
+                options.setIpV6(true);
+            }
+
             // Create the socket
-            socket6 = vertx.createDatagramSocket(options);
+            socket = vertx.createDatagramSocket(options);
 
             //Listen for discovery messages
-            setupDiscoveryListener(startPromise, socket6);
-            
+            setupDiscoveryListener(startPromise, socket);
+
         }
-        
+
         /**
          * Listen on the Multicast Group
-         * 
+         *
          * @param startPromise
-         * @param socket 
+         * @param socket
          */
         private void setupDiscoveryListener(Promise<Void> startPromise, DatagramSocket socket) {
 
             // Listen on all interfaces and broadcast group
-            socket.listen(LISTEN_PORT, LISTEN_ALL_INTERFACES)
+            socket.listen(LISTEN_PORT, LISTEN_ALL_INTERFACE)
                     .onSuccess(result -> {
                         result.listenMulticastGroup(MULTICAST_GROUP, INTERFACE, null)
-                                .onSuccess(v -> LOG.info("Listening for Multicast Messages on IPv6 Group [{}] on local address ({}): {}",MULTICAST_GROUP,INTERFACE,result.localAddress()))
+                                .onSuccess(v -> LOG.info("Listening for Multicast Messages on {} Group [{}] on local address ({}): {}", MODE, MULTICAST_GROUP, INTERFACE, result.localAddress()))
                                 .onFailure(startPromise::fail);
                     })
                     .onFailure(startPromise::fail);
@@ -193,31 +239,31 @@ public class Tester {
          * @param packet
          */
         private void respondToDiscovery(DatagramPacket packet) {
-            LOG.info("Received Discovery Packet from {} with data {}",packet.sender(), packet.data());
-            
+            LOG.info("Received Discovery Packet from {} with data {}", packet.sender(), packet.data());
+
+            // Build response
             JsonObject response = new JsonObject();
             response.put("id", id);
             response.put("date", LocalDateTime.now().toString());
-            
+
             Buffer buffer = response.toBuffer();
 
             // Read the port we are supposed to reply back to
             // this port is the port that the 
             JsonObject toJsonObject = packet.data().toJsonObject();
             int parseInt = toJsonObject.getInteger("responsePort");
-            
-            
-            LOG.info("Sending response back to {}:{} with data - {}",packet.sender().host(), parseInt, buffer);
-            
+
+            LOG.info("Sending response back to {}:{} with data - {}", packet.sender().host(), parseInt, buffer);
+
             // Send back the response using the port number supplied in the request
-            // It's here where it fails using IPv6 on MacOS but works
-            // on Linux
+            // It's here where it works with IPv4 but it fails using IPv6 on MacOS but works
+            // on Linux and Windows
             // I get a "No Route to Host"
-            socket6.send(buffer, parseInt, packet.sender().host())
-                    .onSuccess(result -> LOG.info("Send ok"))
+            socket.send(buffer, parseInt, packet.sender().host())
+                    .onSuccess(result -> LOG.info("Response Sent ok"))
                     .onFailure(Throwable::printStackTrace);
         }
-        
+
     }
-    
+
 }
